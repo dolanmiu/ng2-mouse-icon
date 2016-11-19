9ee1cbb6ae9cab4cb9a4d1724b1274f3258a824f
Dolan Miu <dolan_miu@hotmail.com>
2016-11-19 18:22:28 +0000

    added mouse component code

--- src/mouse-icon.component.ts
+++ src/mouse-icon.component.ts
@@ -1,3 +1,14 @@
-export class MouseIcon {
-    
+import { Component } from '@angular/core';
+import { STYLES } from './templates';
+
+@Component({
+    selector: 'app-mouse-icon',
+    template: `
+        <div class="mouse-icon">
+            <div class="wheel"></div>
+        </div>
+    `,
+    styles: [STYLES]
+})
+export class MouseIconComponent {
 }
